a7ec848a6646ec6923e3b3ef68e4f0e9762b36e7
Faisal Chaudry <faisalchaudryuk@gmail.com>
2018-02-10 19:04:47 +0000

    Added basic un-responsive navigation bar

--- about.php
+++ about.php
@@ -1,10 +1,13 @@
 <?php include("inc/header.php"); ?>
+<?php include("inc/nav.php"); ?>
 <main>
   <div class="main-content">
     <div class="about">
-      <h1>About</h1>
-      <p>I make things and solve problems / things i belive in</p>
-
+      <div class="biography">
+        <h1>about</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
     </div>
   </div>
 </main>
---
+++ inc/nav.php
@@ -0,0 +1,14 @@
+<div class="main-nav">
+  <nav>
+    <a href="/"><?php echo file_get_contents("img/nav-logo.svg"); ?></a>
+  </nav>
+  <!-- <nav>
+  </nav> -->
+  <nav>
+    <a href="#">design</a>
+    <a href="#">engineering</a>
+    <a href="#">audio</a>
+    <a href="#">about</a>
+    <a href="#">contact</a>
+  </nav>
+</div>
